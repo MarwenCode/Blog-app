a7dd08d5469c5088059cfd8c15a5956f9092e447
MarwenCode <marwen_jdidi@yahoo.com>
2022-08-05 00:19:23 +0200

    my blogs & profileSideBar expression update

--- client/src/components/profileSideBar/ProfileSideBar.jsx
+++ client/src/components/profileSideBar/ProfileSideBar.jsx
@@ -1,39 +1,74 @@
-import React from 'react'
-import "./profilesidebar.scss"
+import React, { useState, useEffect } from "react";
+import "./profilesidebar.scss";
+import axios from "axios";
+import { useContext } from "react";
+import { AppContext } from "../../context/context";
 
 const ProfileSideBar = () => {
+  const { user } = useContext(AppContext);
+  const publicFolder = "http://localhost:8000/images/";
+
+  const [expression, setExpression] = useState("");
+  const [inputGroup, setInputGroup] = useState([]);
+  const [updateMode, setUpdateMode] = useState(false);
+
+  const handleExpression = (e) => {
+    e.preventDefault();
+    const newExpression = {
+      id: new Date().getTime().toString(),
+      expression: expression,
+    };
+    setInputGroup([...inputGroup, newExpression]);
+    setExpression("");
+    setUpdateMode(false)
+  };
+
+  console.log(inputGroup);
+
+  const exp = localStorage.getItem("exp");
+  if (exp) {
+    return JSON.parse(localStorage.getItem("exp"));
+  }
+
   return (
-    <div className='ProfileSideBar'>
-        <div className="profileUp">
-            <span className='profilepic'>profilepic</span>
-            <span className='name'>name</span>
-            
-        </div>
-        <div className="profileDown">
-            <div className="profileInfo">
-                <ul className='profileItems'>
-                    <li className='item'>
-                        name
+    <div className="ProfileSideBar">
+      <div className="profileUp">
+        <img className="profilepic" src={publicFolder + user.profilePic} />
+        <span className="name">{user.username}</span>
+      </div>
+      <div className="profileDown">
+        <div className="profileInfo">
+          <i
+            className="singlePostIconEdit far fa-edit"
+            onClick={() => setUpdateMode(true)}></i>
 
-                    </li>
-                    <li className='item'>
-                        age
+          {updateMode && (
+            <form className="profileItems">
+              <textarea
+                className="item"
+                type="text"
+                autoFocus
+                value={expression}
+                onChange={(e) => setExpression(e.target.value)}
+              />
+              <button
+                className="ExpressionBtn"
+                onClick={handleExpression}></button>
+            </form>
+          )} 
 
-                    </li>
-                    <li className='item'>
-                        hobbies
-
-                    </li>
-                    <li className='item'>
-                        expression
-
-                    </li>
-
-                </ul>
+            <div className="inputExpression">
+              {inputGroup.map((exp) => (
+                <div className="expression">
+                  <p> {exp.expression}</p>
+                </div>
+              ))}
             </div>
+          
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileSideBar
+export default ProfileSideBar;
--- client/src/pages/AboutMe/AboutMe.jsx
+++ client/src/pages/AboutMe/AboutMe.jsx
@@ -1,24 +1,21 @@
-
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import ProfileSideBar from "../../components/profileSideBar/ProfileSideBar";
 import "./aboutme.scss";
 
 const AboutMe = () => {
-    const [posts, setPosts] = useState([])
+  const publicForlder = "http://localhost:8000/images/";
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        const fetchPosts = async() => {
-            const res = await axios.get("/post")
-            console.log(res)
-            setPosts(res.data)
-            
-        }
-  
-        fetchPosts()
-    }, [])
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/post");
+      console.log(res);
+      setPosts(res.data);
+    };
 
-
+    fetchPosts();
+  }, []);
 
   return (
     <div className="aboutme">
@@ -29,37 +26,31 @@ const AboutMe = () => {
       <div className="bloc">
         test
         {posts.map((post) => (
-            <>
-             <div className="singlePost">
-          <div className="singlePostWrapper">
-            <img className="singlePostImg" alt="" />
+          <>
+            <div className="singlePost">
+              <div className="singlePostWrapper">
+                <img className="singlePostImg" src={publicForlder + post.photo}  alt="" />
+              
 
+                <div className="singlePostEdit">
+                  <h1 className="singlePostTitle"> {post.title} </h1>
+                  <i className="singlePostIconEdit far fa-edit"></i>
+                  <i className="singlePostIconDelete far fa-trash-alt"></i>
+                </div>
 
-            <h1 className="singlePostTitle"> </h1>
-                {post.title}
-              <div className="singlePostEdit">
-                <i className="singlePostIconEdit far fa-edit"></i>
-                <i className="singlePostIconDelete far fa-trash-alt"></i>
+                <div className="postInfo">
+                  {/* <Link to={`/post/${post._id}`} className="link"> */}
+                  {/* <span className="postTitle">{post.title}</span> */}
+                  {/* </Link> */}
+
+                  <span className="author">{post.username}</span>
+                  <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
+                </div>
+                <p className="postDesc">{post.description}</p>
               </div>
-            
-
-              <div className="postInfo">
-        {/* <Link to={`/post/${post._id}`} className="link"> */}
-          <span className="postTitle">{post.title}</span>
-        {/* </Link> */}
-
-        <span className="author">{post.name}</span>
-        <span className="postDate">{post.createdAt}</span>
-      </div>
-      <p className="postDesc">{post.description}</p>
-
-          
-          </div>
-        </div>
-            </>
-
+            </div>
+          </>
         ))}
-       
       </div>
       <div className="rightSection">test</div>
     </div>
